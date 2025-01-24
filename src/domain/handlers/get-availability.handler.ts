e3131995a189e99b5511bbf56a60586dca73378d
estanip <estani.pettigrew@gmail.com>
2025-01-24 12:19:39 -0300

    feat: improve availabilty search

--- src/domain/handlers/get-availability.handler.ts
+++ src/domain/handlers/get-availability.handler.ts
@@ -1,6 +1,13 @@
-import { Inject } from '@nestjs/common';
+import { BadRequestException, CACHE_MANAGER, Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { Cache } from 'cache-manager';
+import * as moment from 'moment';
 
+import { CACHE_TTL } from '../../shared/constants/cache-ttl.constants';
+import {
+  _addDaysToDate,
+  _getCurrentDate,
+} from '../../shared/helpers/date.helper';
 import {
   ClubWithAvailability,
   GetAvailabilityQuery,
@@ -17,30 +24,61 @@ export class GetAvailabilityHandler
   constructor(
     @Inject(ALQUILA_TU_CANCHA_CLIENT)
     private alquilaTuCanchaClient: AlquilaTuCanchaClient,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async execute(query: GetAvailabilityQuery): Promise<ClubWithAvailability[]> {
-    const clubs_with_availability: ClubWithAvailability[] = [];
+    // improve a date validator, cannot be less than current or more than 8 days from current
+    await this._validateDate(query.date);
+
+    let clubs_with_availability: ClubWithAvailability[] = [];
     const clubs = await this.alquilaTuCanchaClient.getClubs(query.placeId);
-    for (const club of clubs) {
-      const courts = await this.alquilaTuCanchaClient.getCourts(club.id);
-      const courts_with_availability: ClubWithAvailability['courts'] = [];
-      for (const court of courts) {
-        const slots = await this.alquilaTuCanchaClient.getAvailableSlots(
-          club.id,
-          court.id,
-          query.date,
+
+    // Improve flow using paralelism instead a sequential flow & manage erros
+    clubs_with_availability = await Promise.all(
+      clubs.map(async (club) => {
+        // Set club in cache to improve events flow for 2 hours
+        if (this.cacheManager.get(`club:${club.id}`))
+          this.cacheManager.set(`club:${club.id}`, club, CACHE_TTL.CLUBS);
+
+        const courts = await this.alquilaTuCanchaClient.getCourts(club.id);
+
+        const courts_with_availability = await Promise.all(
+          courts.map(async (court) => {
+            try {
+              // Set court in cache to improve events flow for 2 hours
+              if (this.cacheManager.get(`court:${court.id}`))
+                this.cacheManager.set(
+                  `court:${court.id}`,
+                  court,
+                  CACHE_TTL.COURTS,
+                );
+
+              const slots = await this.alquilaTuCanchaClient.getAvailableSlots(
+                club.id,
+                court.id,
+                query.date,
+              );
+
+              return { ...court, available: slots };
+            } catch (error) {
+              // Return a result even if there was an error
+              return { ...court, available: [] };
+            }
+          }),
         );
-        courts_with_availability.push({
-          ...court,
-          available: slots,
-        });
-      }
-      clubs_with_availability.push({
-        ...club,
-        courts: courts_with_availability,
-      });
-    }
+        return { ...club, courts: courts_with_availability };
+      }),
+    );
+
     return clubs_with_availability;
+  }
+
+  private async _validateDate(date) {
+    const currentDate = _getCurrentDate();
+    const nextWeek = _addDaysToDate(new Date(), 7);
+
+    if (moment(date).isBefore(currentDate) || moment(date).isAfter(nextWeek))
+      throw new BadRequestException('Invalid Date');
   }
 }
