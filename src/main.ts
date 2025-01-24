6da51093da22dfb5714e0f6be5590b909ea511ed
estanip <estani.pettigrew@gmail.com>
2025-01-24 12:18:33 -0300

    feat: set filters & interceptors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import {
 } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './infrastructure/exceptions/http.exception';
+import { ErrorInterceptor } from './infrastructure/interceptors/error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter({ logger: false }),
   );
+
+  // Active filters & interceptors
+  app.useGlobalInterceptors(new ErrorInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
