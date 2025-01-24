3b6151bb8341baba7211e74aafa535d599828546
estanip <estani.pettigrew@gmail.com>
2025-01-24 12:15:52 -0300

    feat: add a cache module

---
+++ src/infrastructure/cache/cache.module.ts
@@ -0,0 +1,25 @@
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: configService.get<string>('REDIS_HOST') || 'localhost',
+              port: Number(configService.get<string>('REDIS_PORT')) || 6381,
+            },
+          }),
+        };
+      },
+    }),
+  ],
+})
+export class RedisModule {}
