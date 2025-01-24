0854bd9d6cd8347e6780d98678d195d4da73dfd7
estanip <estani.pettigrew@gmail.com>
2025-01-24 12:17:20 -0300

    Feat: set ConfigModule & RedisModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 
+import { validationSchema } from './configuration/environment/validation.schema';
 import { ClubUpdatedHandler } from './domain/handlers/club-updated.handler';
 import { GetAvailabilityHandler } from './domain/handlers/get-availability.handler';
 import { ALQUILA_TU_CANCHA_CLIENT } from './domain/ports/aquila-tu-cancha.client';
+import { RedisModule } from './infrastructure/cache/cache.module';
 import { HTTPAlquilaTuCanchaClient } from './infrastructure/clients/http-alquila-tu-cancha.client';
 import { EventsController } from './infrastructure/controllers/events.controller';
 import { SearchController } from './infrastructure/controllers/search.controller';
 
 @Module({
-  imports: [HttpModule, CqrsModule, ConfigModule.forRoot()],
+  imports: [
+    HttpModule,
+    CqrsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validate: (config) => validationSchema.parse(config),
+    }),
+    RedisModule,
+  ],
   controllers: [SearchController, EventsController],
   providers: [
     {
